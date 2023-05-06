cb1fa6cdd20ad17c82c1febbe888c3034126f746
ARYAN VERMA <55238063+aryanver2001@users.noreply.github.com>
2023-05-07 00:56:10 +0530

    Update Banking.java

--- Banking.java
+++ Banking.java
@@ -64,3 +64,81 @@ public class Bank {
         return false;
     }
 }
+
+// branch.java
+
+import java.util.ArrayList;
+
+public class Branch {
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean newCustomer(String customerName, double initialTransaction) {
+        if (findCustomer(customerName) == null) {
+            Customer customer = new Customer(customerName, initialTransaction);
+            customers.add(customer);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String customerName, double transaction) {
+        Customer customer = findCustomer(customerName);
+        if (customer != null) {
+            customer.addTransaction(transaction);
+            return true;
+        }
+        return false;
+    }
+
+    private Customer findCustomer(String customerName) {
+        for (Customer customer : customers) {
+            if (customer.getName().equals(customerName)) {
+                return customer;
+            }
+        }
+        return null;
+    }
+}
+
+
+// Customer.java
+
+import java.util.ArrayList;
+
+public class Customer {
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, double initialTransaction) {
+        this.name = name;
+        this.transactions = new ArrayList<>();
+        addTransaction(initialTransaction);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+
+    public void addTransaction(double transaction) {
+        transactions.add(transaction);
+    }
+}
+
